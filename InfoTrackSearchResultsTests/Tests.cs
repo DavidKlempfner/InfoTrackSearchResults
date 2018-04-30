65fc61e4e1e2c3c3cd8c051bcc58a98400bceee5
AVANADE-CORP\david.klempfner <dklempfner@gmail.com>
2018-04-30 13:51:31 +1000

    Using constants in unit tests.

--- InfoTrackSearchResultsTests/Tests.cs
+++ InfoTrackSearchResultsTests/Tests.cs
@@ -11,25 +11,25 @@ namespace InfoTrackSearchResultsTests
         public void GivenKeyWordsAndMaxNumOfResultsExpectCorrectUrl()
         {
             //Arrange
-            string keyWordsSpaceDelimited = "some key words";
-            int maxNumResults = 20;
+            const string KeyWordsSpaceDelimited = "some key words";
+            const int MaxNumResults = 20;
 
             //Act
-            string actualUrl = UrlGenerator.GenerateGoogleSearchUrl(keyWordsSpaceDelimited, maxNumResults);
+            string actualUrl = UrlGenerator.GenerateGoogleSearchUrl(KeyWordsSpaceDelimited, MaxNumResults);
 
             //Assert
-            string expectedUrl = "https://www.google.com.au/search?q=some+key+words&num=20";
-            Assert.AreEqual(actualUrl, expectedUrl);
+            const string ExpectedUrl = "https://www.google.com.au/search?q=some+key+words&num=20";
+            Assert.AreEqual(actualUrl, ExpectedUrl);
         }
 
         [TestMethod]
         public void GivenWebPageContentExpectCorrectUrlsAreExtracted()
         {
             //Arrange
-            string someRandomWebPageContent = "<!doctype html><html><span class=\"Z98Wse\">Ad</span><cite>https://www.infotrack.com.au/Property_<b>Search</b></cite><span class=\"CiacGf\"></span><cite>www.saiglobal.com/<b>title</b>-<b>search</b></cite></div><table border=\"0\" cellpadding=\"0\" cellspacing=\"0\"><tr><td valign=\"top\"></html>";
+            const string SomeRandomWebPageContent = "<!doctype html><html><span class=\"Z98Wse\">Ad</span><cite>https://www.infotrack.com.au/Property_<b>Search</b></cite><span class=\"CiacGf\"></span><cite>www.saiglobal.com/<b>title</b>-<b>search</b></cite></div><table border=\"0\" cellpadding=\"0\" cellspacing=\"0\"><tr><td valign=\"top\"></html>";
             
             //Act
-            List<string> urls = HtmlStringHelper.ExtractUrlsFromWebPageContent(someRandomWebPageContent);
+            List<string> urls = HtmlStringHelper.ExtractUrlsFromWebPageContent(SomeRandomWebPageContent);
 
             //Assert
             Assert.AreEqual("https://www.infotrack.com.au/Property_Search", urls[0]);
@@ -40,26 +40,26 @@ namespace InfoTrackSearchResultsTests
         public void GivenWebPageContentWithInfoTrackResultsExpectCorrectRankings()
         {
             //Arrange
-            string someRandomWebPageContent = "<!doctype html><html><span class=\"Z98Wse\">Ad</span><cite>https://www.infotrack.com.au/Property_<b>Search</b></cite><span class=\"CiacGf\"></span><cite>www.saiglobal.com/<b>title</b>-<b>search</b></cite></div><table border=\"0\" cellpadding=\"0\" cellspacing=\"0\"><tr><td valign=\"top\"><cite>https://www.infotrack.com.au/SomePage</cite></html>";
-            string infoTrackUrl = "https://www.infotrack.com.au";
-            string expectedRankings = "1, 3";
+            const string SomeRandomWebPageContent = "<!doctype html><html><span class=\"Z98Wse\">Ad</span><cite>https://www.infotrack.com.au/Property_<b>Search</b></cite><span class=\"CiacGf\"></span><cite>www.saiglobal.com/<b>title</b>-<b>search</b></cite></div><table border=\"0\" cellpadding=\"0\" cellspacing=\"0\"><tr><td valign=\"top\"><cite>https://www.infotrack.com.au/SomePage</cite></html>";
+            const string InfoTrackUrl = "https://www.infotrack.com.au";
+            const string ExpectedRankings = "1, 3";
 
             //Act
-            string actualRankings = ResultsGenerator.GetRankingsFormattedAsString(someRandomWebPageContent, infoTrackUrl);
+            string actualRankings = ResultsGenerator.GetRankingsFormattedAsString(SomeRandomWebPageContent, InfoTrackUrl);
 
             //Assert
-            Assert.AreEqual(expectedRankings, actualRankings);
+            Assert.AreEqual(ExpectedRankings, actualRankings);
         }
 
         [TestMethod]
         public void GivenWebPageContentWithNoInfoTrackResultsExpectCorrectRankings()
         {
             //Arrange
-            string someRandomWebPageContent = "<!doctype html><html><span class=\"Z98Wse\">Ad</span><cite>https://www.website1.com.au/Property_<b>Search</b></cite><span class=\"CiacGf\"></span><cite>www.saiglobal.com/<b>title</b>-<b>search</b></cite></div><table border=\"0\" cellpadding=\"0\" cellspacing=\"0\"><tr><td valign=\"top\"><cite>https://www.website2.com.au/SomePage</cite></html>";
-            string infoTrackUrl = "https://www.infotrack.com.au";
+            const string SomeRandomWebPageContent = "<!doctype html><html><span class=\"Z98Wse\">Ad</span><cite>https://www.website1.com.au/Property_<b>Search</b></cite><span class=\"CiacGf\"></span><cite>www.saiglobal.com/<b>title</b>-<b>search</b></cite></div><table border=\"0\" cellpadding=\"0\" cellspacing=\"0\"><tr><td valign=\"top\"><cite>https://www.website2.com.au/SomePage</cite></html>";
+            const string InfoTrackUrl = "https://www.infotrack.com.au";
 
             //Act
-            string actualRankings = ResultsGenerator.GetRankingsFormattedAsString(someRandomWebPageContent, infoTrackUrl);
+            string actualRankings = ResultsGenerator.GetRankingsFormattedAsString(SomeRandomWebPageContent, InfoTrackUrl);
 
             //Assert
             Assert.AreEqual("0", actualRankings);
